64cbcb20a7fcf11175c6dfedf364f0b838a5dae9
Tasio Almansa <at.almansa@gmail.com>
2017-04-28 14:06:00 +0200

    Cambiamos el didFinishLaunched a privado en el AppDelegate según cambios en beta 6

--- ParseServerStarterProject 2/ParseStarterProject/AppDelegate.swift
+++ ParseServerStarterProject 2/ParseStarterProject/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - UIApplicationDelegate
     //--------------------------------------
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Enable storing and querying data from Local Datastore.
         // Remove this line if you don't want to use Local Datastore features or want to use cachePolicy.
         Parse.enableLocalDatastore()
